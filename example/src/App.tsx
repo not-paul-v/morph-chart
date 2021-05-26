79db52a17dfb5e0c7b6c4918c53eddd7737c27b3
itspauv <p.viehmann01@gmail.com>
2021-05-26 18:29:01 +0200

    added styling + chart buttons

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,7 +4,7 @@ import { Chart } from 'lean-chart'
 import 'lean-chart/dist/index.css'
 
 const App = () => {
-  return <Chart width={800} height={400} />
+  return <Chart width={800} height={400} chartTypes={[0, 1, 2, 3]} />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
-// import styles from './styles.module.css'
+import styles from "./styles.module.css";
 import { calcData } from './lib/model';
 import { graphData } from "./testGraphData";
 
 type ChartProps = {
   width: number,
-  height: number
+  height: number,
+  chartTypes: any[] | null
 }
 
-export const Chart = ({ width, height }: ChartProps) => {
+export const Chart = ({ width, height, chartTypes }: ChartProps) => {
   const pathData = calcData(graphData, 0, width, height);
   const currentPathString = pathData.path;
 
   return(
-    <div id="chartContainer">
+    <div className={styles.chartContainer} style={{ width, height }}>
       <svg width={width} height={height}>
-        <path d={ pathData !== null ? currentPathString : "" }/>
+        <path d={pathData !== null ? currentPathString : ""} style={{ fill: "transparent", stroke: "blue", strokeWidth: 4 }} />
       </svg>
+      { chartTypes === null ? null :
+        <div className={styles.buttonContainer}>
+          {chartTypes.map((value, index) => (
+            <button className={styles.chartButton} key={index}>{value}</button>
+          ))}
+        </div>
+      }
     </div>
   );
 }
