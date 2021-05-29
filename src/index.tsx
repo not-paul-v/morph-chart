8603399bd4ed7ca53c9ded537e5bc9a701eb500a
itspauv <p.viehmann01@gmail.com>
2021-05-29 14:16:19 +0200

    added graph data types

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,9 @@ import React from 'react';
 import styles from "./styles.module.css";
 import { calcData } from './lib/model';
 import { graphData } from "./testGraphData";
+import { ChartProps } from './types/types';
 
-type ChartProps = {
-  width: number,
-  height: number,
-  chartTypes: any[] | null
-}
-
-export const Chart = ({ width, height, chartTypes }: ChartProps) => {
+export const Chart = ({ width, height, data }: ChartProps) => {
   const pathData = calcData(graphData, 0, width, height);
   const currentPathString = pathData.path;
 
@@ -18,9 +13,9 @@ export const Chart = ({ width, height, chartTypes }: ChartProps) => {
       <svg width={width} height={height}>
         <path d={pathData !== null ? currentPathString : ""} style={{ fill: "transparent", stroke: "blue", strokeWidth: 4 }} />
       </svg>
-      { chartTypes === null ? null :
+      { data.chartLabels === null ? null :
         <div className={styles.buttonContainer}>
-          {chartTypes.map((value, index) => (
+          {data.chartLabels.map((value, index) => (
             <button className={styles.chartButton} key={index}>{value}</button>
           ))}
         </div>
---
+++ src/types/types.ts
@@ -0,0 +1,32 @@
+export type ChartProps = {
+    width: number;
+    height: number;
+    data: ChartData;
+};
+
+export type DataPoint = {
+    value: number;
+    label: string | null;
+};
+
+export type DataPoints = {
+    maxDataPoints: number | undefined;
+    dataPoints: DataPoint[];
+};
+
+export type ChartData = {
+    chartLabels: string[] | null;
+    // chart data length has to match chartTypes length if > 1
+    chartData: DataPoints[] | null;
+    // e.g. ticker
+    title?: string | null;
+    // e.g. currency
+    subtitle?: string | null;
+    // if true display value of current mouse position
+    // if false displays last value
+    displayCurrentValue: boolean;
+    displayPercentageChange: boolean;
+    displayPointLabels: boolean;
+    // if true checks for max datapoints prop to display partial graph otherwise stretches graph to full width
+    partialGraph?: boolean;
+};
