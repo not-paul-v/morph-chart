af9c2ac707c0ef5f65b7d36aba4d80d1288e51ce
itspauv <p.viehmann01@gmail.com>
2021-05-29 17:41:23 +0200

    added dummy data

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,8 +3,17 @@ import React from 'react'
 import { Chart } from 'lean-chart'
 import 'lean-chart/dist/index.css'
 
+const data = {
+  chartLabels: ["1D", "5D", "1M", "1Y"],
+  //todo type export
+  chartData: [{maxDataPoints: 70, dataPoints: [0, "t"]} as any],
+  displayCurrentValue: true,
+  displayPercentageChange: true,
+  displayPointLabels: false,
+}
+
 const App = () => {
-  return <Chart width={800} height={400} chartTypes={[0, 1, 2, 3]} />
+  return <Chart width={800} height={400} data={data} />
 }
 
 export default App
